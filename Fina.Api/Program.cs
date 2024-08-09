3dfdd0ed3293180fb94821994a84b7d15a2a962f
fjastre <fsjastre@apoioprodesp.sp.gov.br>
2024-08-09 14:40:43 -0300

    Dia 2

--- Fina.Api/Program.cs
+++ Fina.Api/Program.cs
@@ -1,4 +1,20 @@
+using Fina.Api.Data;
+using Fina.Api.Handlers;
+using Fina.Core.Handlers;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+const string connectionString = 
+    "Server=localhost\\SQLEXPRESS;Database=Fina;Trusted_Connection=true;TrustServerCertificate=True;";
+
+builder.Services.AddDbContext<AppDbContext>(
+    x => x.UseSqlServer(connectionString)
+);
+
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
