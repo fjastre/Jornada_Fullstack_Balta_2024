faecf506fdc2c1fde6e8c0120b9fcd3069e97007
fjastre <fsjastre@apoioprodesp.sp.gov.br>
2024-08-25 20:15:48 -0300

    Dia 2 01:34

--- Fina.Api/Handlers/CategoryHandler.cs
+++ Fina.Api/Handlers/CategoryHandler.cs
@@ -3,33 +3,126 @@ using Fina.Core.Handlers;
 using Fina.Core.Models;
 using Fina.Core.Requests.Categories;
 using Fina.Core.Responses;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fina.Api.Handlers;
 
 public class CategoryHandler(AppDbContext context) : ICategoryHandler
 {
-    public Task<Response<Category?>> CreateAsync(CreateCategoryRequest request)
+    public async Task<Response<Category?>> CreateAsync(CreateCategoryRequest request)
     {
-        throw new NotImplementedException();
+        var category = new Category()
+        {
+            UserId = request.UserId,
+            Title = request.Title,
+            Description = request.Description
+        };
+
+        try
+        {
+            await context.Categories.AddAsync(category);
+            await context.SaveChangesAsync();
+
+            return new Response<Category?>(category, code: 201, message: "Categoria criada com sucesso!");
+        }
+        catch
+        {
+            return new Response<Category?>(null, code: 500, message: "Não foi possível criar a categoria.");
+        }
     }
 
-    public Task<Response<Category?>> DeleteAsync(DeleteCategoryRequest request)
+    public async Task<Response<Category?>> UpdateAsync(UpdateCategoryRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var category = await context
+                .Categories
+                .FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+            if (category is null)
+                return new Response<Category?>(null, code: 404, message: "Categoria não encontrada.");
+
+            category.Title = request.Title;
+            category.Description = request.Description;
+
+            context.Categories.Update(category);
+            await context.SaveChangesAsync();
+
+            return new Response<Category?>(category, message: "Categoria atualizada com sucesso!");
+        }
+        catch
+        {
+            return new Response<Category?>(null, code: 500, message: "Não foi possível atualizar a categoria.");
+        }
     }
 
-    public Task<PagedResponse<List<Category>?>> GetAllAsync(GetAllCategories request)
+    public async Task<Response<Category?>> DeleteAsync(DeleteCategoryRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var category = await context
+                .Categories
+                .FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+            if (category is null)
+                return new Response<Category?>(null, code: 404, message: "Categoria não encontrada.");
+
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+
+            return new Response<Category?>(category, message: "Categoria excluída com sucesso!");
+        }
+        catch
+        {
+            return new Response<Category?>(null, code: 500, message: "Não foi possível excluir a categoria.");
+        }
     }
 
-    public Task<Response<Category?>> GetByIdAsync(GetCategoryByIdRequest request)
+    public async Task<PagedResponse<List<Category>?>> GetAllAsync(GetAllCategories request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var query = context
+                .Categories
+                .AsNoTracking()
+                .Where(x => x.UserId == request.UserId)
+                .OrderBy(x => x.Title);
+
+            var categories = await query
+                .Skip((request.PageNumber - 1) * request.PageSize)
+                .Take(request.PageSize)                
+                .ToListAsync();
+
+            var count = await query.CountAsync();
+
+            return new PagedResponse<List<Category>?>(
+                categories,
+                count,
+                request.PageNumber,
+                request.PageSize);
+        }
+        catch
+        {
+            return new PagedResponse<List<Category>?>(null, code: 500, message: "Não foi possível retornar as categorias.");
+        }
     }
 
-    public Task<Response<Category?>> UpdateAsync(UpdateCategoryRequest request)
+    public async Task<Response<Category?>> GetByIdAsync(GetCategoryByIdRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var category = await context
+                .Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+            return category is null
+                ? new Response<Category?>(data: null, code: 404, message: "Categoria não encontrada.")
+                : new Response<Category?>(category);
+        }
+        catch
+        {
+            return new Response<Category?>(null, code: 500, message: "Não foi possível obter a categoria.");
+        }
     }
 }
