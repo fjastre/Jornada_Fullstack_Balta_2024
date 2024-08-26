7c8ec5cb4ec37259c95fdc3f024d567fd9928cf8
fjastre <fsjastre@apoioprodesp.sp.gov.br>
2024-08-25 21:32:39 -0300

    Dia 2 01:51

---
+++ Fina.Api/Endpoints/Categories/GetCategoryByIdEndpoint.cs
@@ -0,0 +1,5 @@
+﻿namespace Fina.Api.Endpoints.Categories;
+
+public class GetCategoryByIdEndpoint
+{
+}
---
+++ Fina.Api/Endpoints/Transactions/GetTransactionByIdEndpoint.cs
@@ -0,0 +1,5 @@
+﻿namespace Fina.Api.Endpoints.Transactions;
+
+public class GetTransactionByIdEndpoint
+{
+}
